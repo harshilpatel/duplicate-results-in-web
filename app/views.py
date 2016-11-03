ae3f77b90a4f8cb03acc7a78205b8273e0669d4f
harshilpatel <harshil912@gmail.com>
2016-11-03 19:18:15 +0530

    working well

--- app/views.py
+++ app/views.py
@@ -1,51 +1,51 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import string
+import logging
+
+def int_to_string(a):
+	a = str(a).strip()
+	if len(a) == 1:
+		return "0" + a
+	else:
+		return a
 
 def string_to_hex(request):
 	a = request.GET.get('string')
+	a = a.replace('+',' ')
 	a = a.lower()
 
 	a = a.replace(' ','')
 	if len(a)%3 != 0:
-		a += a[-1]*(3 - len(a)%3)
+		a += str(a[-1])*(3 - len(a)%3)
 
-	a_list = []
+	a_n = []
 
 	for i in range(0,len(a),3):
-		a_list.append(a[i:i+3])
+		a_n.append(a[i:i+3])
 
-
-	alpha = list(string.ascii_lowercase)
+	alpha = list(string.ascii_lowercase) + [str(i) for i in range(10)]
+	# print alpha
 	number = list(range(0,len(alpha)))
-
-
-
-	values = []
-	for i in a_list:
+	values = [0,1,2]
+	for i in a_n:
+		if len(i) != 3:
+			logging.error("Length  not as expected " + i)
 		hex_value = ''
-		temp = [ str(number[alpha.index(ic)])  for ic in list(i)]
-		for j in range(0,len(temp)):
-			if len(temp[j]) == 1:
-				temp[j] = '0' + temp[j]
+		temp = [ number[alpha.index(str(ic))] for ic in i]
+		# print temp
+		# print values
+		for j in range(0,3):
+			values[j] =  values[j] + temp[j]
+			values[j] = int(int_to_string(values[j])[:2])
 
-			hex_value2 = hex(int(temp[j]))[2:]
-			if len(hex_value2) == 1:
-				hex_value2 = '0' + hex_value2
-			hex_value += hex_value2
-
-		if len(values)>0:
-			for i in range(0,3):
-				values[i] =  str(int(values[i]) * int(temp[i]))[:2]
-		else:
-			values = temp
-
+	# print values
 	hex_value = ''
 	for i in values:
-		hex_value2 = hex(int(i))[2:]
-		if len(hex_value2) == 1:
-			hex_value2 =  '0'+ hex_value2
+		hex_value2 = str(hex(int(i))[2:])
+		hex_value2 = int_to_string(hex_value2)
 		hex_value += hex_value2
+	# print hex_value
 
 	return JsonResponse({'value': hex_value})
 
